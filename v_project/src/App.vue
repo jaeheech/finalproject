597848a517cf044f92f46ee945f9b3dfd755053c
jaeheech <ks2451ss1@naver.com>
2023-08-09 17:40:04 +0900

    final

--- v_project/src/App.vue
+++ v_project/src/App.vue
@@ -1,33 +1,34 @@
 <template>
-  <header style="margin: 20px 0">
-    <img src="../public/logo.jpg" alt="logo" />
-  </header>
-  <nav>
-    <router-link to="/">메인</router-link>
-    <router-link to="/about">부위별 운동</router-link>
-    <router-link to="/about">나만의 루틴</router-link>
-    <router-link to="/about">집근처 헬스장</router-link>
-    <router-link to="/about">각종 구매처</router-link>
-    <router-link to="/about" style="margin-right: 80px">자유게시판</router-link>
-    |
-    <router-link to="/about" style="margin-left: 80px">로그인</router-link>
-    <router-link to="/about">마이페이지</router-link>
-  </nav>
-  <router-view style="height: 60%" />
-  <Footer>
-    <div id="footer_container">
-      <div id="footer_up">
-        <div id="Inf"></div>
-        <div id="Adv"></div>
+  <div>
+    <header>
+      <img src="../public/logo.jpg" alt="logo" />
+    </header>
+    <nav class="main-nav">
+      <router-link to="/" class="nav-link">메인</router-link>
+      <router-link to="/about" class="nav-link">부위별 운동</router-link>
+      <router-link to="/routine" class="nav-link">나만의 루틴</router-link>
+      <router-link to="/gyms" class="nav-link">집근처 헬스장</router-link>
+      <router-link to="/purchases" class="nav-link">각종 구매처</router-link>
+      <router-link to="/board" class="nav-link">자유게시판</router-link>
+      <router-link to="/login" class="nav-link right-link">로그인</router-link>
+      <router-link to="/mypage" class="nav-link">마이페이지</router-link>
+    </nav>
+    <router-view style="height: 60%" />
+    <Footer>
+      <div id="footer_container">
+        <div id="footer_up">
+          <div id="Inf"></div>
+          <div id="Adv"></div>
+        </div>
+        <div id="footer_down">
+          <small>
+            Copyright ⓒ Ministry of Patriots and Veterans Affairs. All Rights
+            Reserved
+          </small>
+        </div>
       </div>
-      <div id="footer_down">
-        <small
-          >Copyright ⓒ Ministry of Patriots and Veterans Affairs. All Rights
-          Reserved</small
-        >
-      </div>
-    </div>
-  </Footer>
+    </Footer>
+  </div>
 </template>
 
 <style>
@@ -50,29 +51,48 @@ header {
   color: #2c3e50;
 }
 
-nav {
+.main-nav {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  align-items: center;
   font-size: 20px;
   color: #585656;
-  width: 100%;
-  height: 10%;
   background-color: #d9d9d9;
+  padding: 10px 20px;
 }
 
-nav a {
-  font-size: 16px;
-  height: 100%;
-  float: left;
-  margin: 0 20px;
-  font-weight: bold;
+.nav-link {
+  text-decoration: none;
   color: #2c3e50;
+  font-weight: bold;
+  transition: background-color 0.3s, color 0.3s;
+  margin: 10px 0;
 }
 
-nav a.router-link-exact-active {
-  width: 10%;
+.nav-link:hover {
   background-color: #585656;
   color: white;
+}
+
+.right-link {
+  margin-left: auto;
+}
+.main-nav .nav-link {
+  text-decoration: none;
+  color: #2c3e50;
   font-weight: bold;
+  transition: background-color 0.3s, color 0.3s;
+  margin: 10px; /* 위아래 10px, 좌우 10px로 수정 */
+  padding: 5px 10px; /* 좌우 패딩 추가 */
+}
+
+.main-nav .nav-link:hover {
+  background-color: #585656;
+  color: white;
+}
+
+.main-nav .right-link {
+  margin-left: auto;
+  margin-right: 0;
 }
 </style>
