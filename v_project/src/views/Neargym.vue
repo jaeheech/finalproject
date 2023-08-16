e940d2f1bcce6278688b81bd086a9dba2eac9073
Yabuinu98 <k4uk4u@naver.com>
2023-08-16 17:23:57 +0900

    tpj

--- v_project/src/views/Neargym.vue
+++ v_project/src/views/Neargym.vue
@@ -16,11 +16,14 @@
       <div id="neargym_content_select">
         우리집 근처 역은?
         <select name="" id="" v-model="station">
-          <option value="">동대신동역</option>
-          <option value="">부산진역</option>
-          <option value="">토성역</option>
+          <option value="동대신동역">동대신동역</option>
+          <option value="토성역">토성역</option>
+          <option value="부산진역">부산진역</option>
+          <option value="서면역">서면역</option>
         </select>
+        <button @click="displayMarker">이동</button>
       </div>
+      <div id="map" class="map-container"></div>
     </div>
   </div>
 </template>
@@ -29,6 +32,60 @@ export default {
   data() {
     return {
       station: ''
+    }
+  },
+  mounted() {
+    const gpsx = 129.0578
+    const gpsy = 35.1578
+    const mapContainer = document.getElementById('map')
+
+    const script = document.createElement('script')
+    script.src =
+      'https://dapi.kakao.com/v2/maps/sdk.js?appkey=e139f221a53ae8e1de064297bd6fbdd1&autoload=false'
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        const mapOptions = {
+          center: new window.kakao.maps.LatLng(gpsx, gpsy),
+          level: 4
+        }
+
+        const map = new window.kakao.maps.Map(mapContainer, mapOptions)
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude
+            const lon = position.coords.longitude
+            const locPosition = new window.kakao.maps.LatLng(lat, lon)
+            const message = '<div>여기가 현재위치</div>'
+            this.displayMarker(map, locPosition, message)
+          })
+        } else {
+          alert(
+            '이 문장은 사용상의 웹 브라우저가 Geolocation API를 지원하지 않을 때 나타납니다.'
+          )
+        }
+      })
+    }
+
+    document.body.appendChild(script)
+  },
+
+  methods: {
+    displayMarker(map, locPosition, message) {
+      const marker = new window.kakao.maps.Marker({
+        map: map,
+        position: locPosition
+      })
+
+      const iwContent = message
+      const iwRemovable = true
+      const infowindow = new window.kakao.maps.InfoWindow({
+        content: iwContent,
+        removable: iwRemovable
+      })
+
+      infowindow.open(map, marker)
+      map.setCenter(locPosition)
     }
   }
 }
@@ -61,5 +118,10 @@ export default {
   margin: 30px;
   width: 85%;
   height: 92%;
+}
+.map-container {
+  width: 800px;
+  height: 300px;
+  padding: 100px;
 }
 </style>
