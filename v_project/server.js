dd764a0c65f64d15ecec6bce141e0a9ab1791f1c
Kimjw111 <whale960105@gmail.com>
2023-08-18 14:12:21 +0900

    tpj

--- v_project/server.js
+++ v_project/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const logger = require('morgan')
 const path = require('path')
 const VSchema = require('./mdb.cjs')
 const app = express()
+app.use(history())
 const _path = path.join(__dirname, './dist')
 // POST를 위한 구문
 app.use(express.json())
