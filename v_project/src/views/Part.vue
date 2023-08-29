a904f25e044a0adf2044d978bde65eb2ae01b7fc
Yabuinu98 <k4uk4u@naver.com>
2023-08-29 16:07:09 +0900

    tpj

--- v_project/src/views/Part.vue
+++ v_project/src/views/Part.vue
@@ -1,13 +1,42 @@
 <template>
   <div id="part_all">
+    <!-- 좌측 네비 파트 -->
     <div id="part_sidebar">
       <ul>
         <li v-for="v in sideBar" :key="v">{{ v }}</li>
       </ul>
     </div>
+    <!-- // 좌측 네비 파트 -->
+
+    <!-- 우측 content 파트 -->
     <div id="part_content">
-      <h1>부위별 운동</h1>
+      <div id="part_content_title" style="border-bottom: 3px solid #989898">
+        <h1 style="margin-left: 1%; text-align: center; letter-spacing: 2px">
+          부위별 운동
+        </h1>
+      </div>
+      <!-- selection 파트 -->
+      <div id="part_content_selection">
+        <input type="checkbox" id="shoulders" value="어깨" />어깨
+        <input type="checkbox" id="chest" value="가슴" />가슴
+        <input type="checkbox" id="back" value="등" />등
+        <input type="checkbox" id="lower_body" value="하체" />하체
+        <input type="checkbox" id="biceps" value="이두" />이두
+        <input type="checkbox" id="triceps" value="삼두" />삼두
+      </div>
+      <!-- // selection 파트 -->
+
+      <!-- result 파트 -->
+      <div id="part_content_result">
+        <div style="background-color: plum; width: 1000px; height: 600px">
+          <p>● 결과들이 카드 형태로 출력될 예정</p>
+          <p>● selection 파트에서 부위를 고르면 필터기능을 구현할 예정</p>
+          <p>● 클릭시 모달창으로 상세정보를 볼 수 있게 구현할 예정</p>
+        </div>
+      </div>
+      <!-- // result 파트 -->
     </div>
+    <!-- // 우측 content 파트 -->
   </div>
 </template>
 <script>
