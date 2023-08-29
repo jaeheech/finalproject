d9f0ef2b81c3ae6afa0ebf0bca358f550d3d89eb
Yabuinu98 <k4uk4u@naver.com>
2023-08-29 16:25:07 +0900

    tpj

---
+++ v_project/public/GymEquipment.js
@@ -0,0 +1,129 @@
+export default [
+  {
+    image: './smithmh.jpg',
+    name: '프론트 라이즈',
+    part: '어깨',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '사이드 레터럴 라이즈',
+    part: '어깨',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '숄더 프레스',
+    part: '어깨',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '체스트 프레스',
+    part: '가슴',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '벤치 프레스',
+    part: '가슴',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '펙덱 플라이',
+    part: '가슴',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '랫풀 다운',
+    part: '등',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '바벨 로우',
+    part: '등',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '시티드 로우',
+    part: '등',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '풀업',
+    part: '등',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '바벨컬',
+    part: '이두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '덤벨컬',
+    part: '이두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '케이블컬',
+    part: '이두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '케이블 푸쉬다운(-형)',
+    part: '삼두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '케이블 푸쉬다운(^형)',
+    part: '삼두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '딥스',
+    part: '삼두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '라잉 트라이셉스 익스텐션',
+    part: '삼두',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '데드리프트',
+    part: '코어',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '스퀴트',
+    part: '하체',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '',
+    part: '하체',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  },
+  {
+    image: './smithmh.jpg',
+    name: '',
+    part: '하체',
+    disciption: '12 ~ 15회 / 4 ~ 5세트'
+  }
+  // 나머지 상품 데이터도 추가
+]
