97146e93a3c8c18ffa1847a53304461d21140bdf
Yabuinu98 <k4uk4u@naver.com>
2023-08-30 10:50:52 +0900

    tpj

--- v_project/public/GymEquipment.js
+++ v_project/public/GymEquipment.js
@@ -1,126 +1,126 @@
 export default [
   {
-    image: './smithmh.jpg',
+    image: './frontRaise.jpg',
     name: '프론트 라이즈',
     part: '어깨',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './sideLateralRaise.jpg',
     name: '사이드 레터럴 라이즈',
     part: '어깨',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './shoulderPress.jpg',
     name: '숄더 프레스',
     part: '어깨',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './chestPress.jpg',
     name: '체스트 프레스',
     part: '가슴',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './benchPress.jpg',
     name: '벤치 프레스',
     part: '가슴',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './pectorFly.jpg',
     name: '펙덱 플라이',
     part: '가슴',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './letPullDown.jpg',
     name: '랫풀 다운',
     part: '등',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './babelLow.jpg',
     name: '바벨 로우',
     part: '등',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './sitedLow.jpg',
     name: '시티드 로우',
     part: '등',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './pullUp.jpg',
     name: '풀업',
     part: '등',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './babelkul.jpg',
     name: '바벨컬',
     part: '이두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './dumdbelkul.jpg',
     name: '덤벨컬',
     part: '이두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './kabelkul.jpg',
     name: '케이블컬',
     part: '이두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './kabelPushDown01.jpg',
     name: '케이블 푸쉬다운(-형)',
     part: '삼두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './kabelPushDown02.jpg',
     name: '케이블 푸쉬다운(^형)',
     part: '삼두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './deeps.jpg',
     name: '딥스',
     part: '삼두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './lieingTricepce.jpg',
     name: '라잉 트라이셉스 익스텐션',
     part: '삼두',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './deadLift.jpg',
     name: '데드리프트',
     part: '코어',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './squart.jpg',
     name: '스퀴트',
     part: '하체',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './legExpress.jpg',
     name: '레그익스프레스',
     part: '하체',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
   },
   {
-    image: './smithmh.jpg',
+    image: './legExtention.jpg',
     name: '레그 익스텐션',
     part: '하체',
     disciption: '12 ~ 15회 / 4 ~ 5세트'
